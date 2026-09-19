060e13752da186f966edf2076d182300476aed2b
Bhavesh Kuranjekar <bhaveshkuranjekar070@gmail.com>
2026-09-19 22:22:14 +0530

    docs: Add structured documentation and formatted query for problem 0175

--- 0175-combine-two-tables/0175-combine-two-tables.sql
+++ 0175-combine-two-tables/0175-combine-two-tables.sql
@@ -1,3 +1,21 @@
-/* Write your PL/SQL query statement below */
-select p.firstName, p.lastName, a.city, a.state from person p left join address a on (p.personId=a.personId)
-/* try to reduce time complexity with () */
+/*
+ * Problem: 175. Combine Two Tables
+ * Difficulty: Easy
+ * LeetCode Link: https://leetcode.com/problems/combine-two-tables/
+ * Topic: Database (SQL)
+ *
+ * Approach:
+ * - Perform a LEFT OUTER JOIN from 'Person' to 'Address' on matching 'personId'.
+ * - A LEFT JOIN preserves all records from the 'Person' table regardless of whether
+ *   an associated address exists in the 'Address' table.
+ * - If no matching 'personId' is found in 'Address', NULL values are automatically
+ *   populated for 'city' and 'state'.
+ */
+SELECT 
+    p.firstName, 
+    p.lastName, 
+    a.city, 
+    a.state 
+FROM Person p 
+LEFT JOIN Address a 
+    ON p.personId = a.personId;
